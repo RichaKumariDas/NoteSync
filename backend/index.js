a94548f643cb7fbaa5ef2dbac96dafb4f17c1d1d
RichaKumariDas <richadas9801@gmail.com>
2025-09-01 01:57:51 +0530

    Refresh all files for submission

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,8 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
+
+
 // app.get('/', (req, res) => {
 //   res.send('Welcome to iNotebook backend!');
 // });
